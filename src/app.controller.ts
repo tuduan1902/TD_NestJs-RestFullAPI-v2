56982e3b2bfdb2d2b9eaab629eb0f35b351feab1
Tu Duan <uuxinhxinh2701@gmail.com>
2022-06-11 00:38:50 +0700

    Create data.ts

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common'
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { data, ReportType } from 'src/data';
 
 @Controller('report/:type')
 export class AppController {
   @Get()
-  getAllReports() {
-    return 'Load all report';
+  getAllReports(@Param('type') type: string) {
+    const reportType = type === 'income' ? ReportType.INCOME : ReportType.EXPENSE
+    return data.report.filter((report) => report.type === reportType);
   }
 
   @Get(':id')
