314dd6c2b5b4bca13a6c348d4bf86576307b00d6
Tu Duan <uuxinhxinh2701@gmail.com>
2022-06-10 23:22:21 +0700

    Create method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,31 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common'
 
-@Controller()
-export class AppController {}
+@Controller('report/:type')
+export class AppController {
+  @Get()
+  getAllReports() {
+    return 'Load all report';
+  }
+
+  @Get(':id')
+  getReportById() {
+    return 'Thanks for report';
+  }
+
+  @Post()
+  createReport() {
+    return 'Created';
+  }
+
+  @Put(':id')
+  updateReport() {
+    return 'updated';
+  }
+
+  @Delete(':id')
+  deleteReport() {
+    return 'delete';
+  }
+}
+
 
